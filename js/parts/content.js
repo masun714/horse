fe961a024c955570fd73e104392246c717ce44e3
masun714 <30792159+masun714@users.noreply.github.com>
2019-12-05 13:01:32 +0800

    Add files via upload

--- js/parts/content.js
+++ js/parts/content.js
@@ -255,7 +255,7 @@ Racing.contentControl = {
         $.each(data, function(k,v){
             html += '<div class="horseInfoRow" rel="' +　v.hrname_c +　'">' 
                 + '<div class="horseName close" rel="' +　v.hrname_c +　'"><span class="openInfo">+</span>' 
-                    + '<a target="_blank" href="https://racing.hkjc.com/racing/information/Chinese/Horse/Horse.aspx?HorseNo=' + v.hrid + '">' + ( k + 1 ) + '.' + v.hrname_c + (horseCurrentMatchStackData.noHistory[v.hrname_c] ? 'H' : '' ) + (horseCurrentMatchStackData.firstTime[v.hrname_c] ? 'F' : '' ) + '</a>'
+                    + '<a target="_blank" href="https://racing.hkjc.com/racing/information/Chinese/Horse/Horse.aspx?HorseNo=' + v.hrid + '">' + v.hrno + '.' + v.hrname_c + (horseCurrentMatchStackData.noHistory[v.hrname_c] ? 'H' : '' ) + (horseCurrentMatchStackData.firstTime[v.hrname_c] ? 'F' : '' ) + '</a>'
                     + '<span class="horseInfoDetail"><br>歲:' + v.age + '<br>騎:' + v.jockey
                     + '<br>負磅' + v.weight
                     + '<br>練:' + v.stable + '<br>趨勢:' + Racing.Function.horseStatus(horseCurrentMatchStackData.status[v.hrname_c]) + '</span>'
