e52c592c51a7cda225d8825915d9ce87efd12d9e
masun714 <30792159+masun714@users.noreply.github.com>
2020-01-03 11:17:23 +0800

    Add files via upload

--- js/parts/dataLoader.js
+++ js/parts/dataLoader.js
@@ -4,36 +4,78 @@ Racing.dataLoader = {
         
         var alldata = '';
     
-        $.ajax({
-            url: 'data/match_all.js',
-            dataType: 'json',
-            async: false,
-            type: "GET",
-            success: function(data){
-                const alldata = data[0];
-                allMatchData = data[0];
-                let horseDataMatch = 1;
+        // $.ajax({
+        //     url: 'data/match_all.js',
+        //     dataType: 'json',
+        //     async: false,
+        //     type: "GET",
+        //     success: function(data){
+        //         const alldata = data[0];
+        //         allMatchData = data[0];
+        //         let horseDataMatch = 1;
                 
-                $.each(alldata, function(content,v){
-                    if(content != 'matchSchedule'){
+        //         $.each(alldata, function(content,v){
+        //             if(content != 'matchSchedule'){
                         
-                        currentMatchHorseData[horseDataMatch] = {}
+        //                 currentMatchHorseData[horseDataMatch] = {}
                         
-                        $.each(v, function(k, horseName){
-                            currentMatchHorseData[horseDataMatch][horseName.hrname_c] = [];
-                        })
+        //                 $.each(v, function(k, horseName){
+        //                     currentMatchHorseData[horseDataMatch][horseName.hrname_c] = [];
+        //                 })
                     
-                        horseDataMatch ++;
+        //                 horseDataMatch ++;
                     
-                    }
-                })
+        //             }
+        //         })
                 
-            }
-        })
+        //     }
+        // })
         
         var getdata = firebase_database.ref('/').once('value', function(data){
             
             var fireData = data.val();
+            
+            console.log(fireData);
+            
+            var allMatch = {};
+            var sortList = [];
+            
+            $.each(fireData.match_all[0], function(k,v){
+                sortList.push(k);
+            })
+            
+            sortList.sort((a,b)=>{
+                return a.replace('match','') - b.replace('match','');
+            })
+            
+            
+            $.each(sortList, function(k,v){
+                
+                allMatch[v] = fireData.match_all[0][v];
+                
+            })
+            
+            
+            console.log(allMatch);
+            
+            const alldata = allMatch;
+            allMatchData = allMatch;
+            let horseDataMatch = 1;
+            
+            $.each(alldata, function(content,v){
+                if(content != 'matchSchedule'){
+                    
+                    currentMatchHorseData[horseDataMatch] = {}
+                    
+                    $.each(v, function(k, horseName){
+                        currentMatchHorseData[horseDataMatch][horseName.hrname_c] = [];
+                    })
+                
+                    horseDataMatch ++;
+                
+                }
+            })
+            
             
             $.each(currentMatchHorseData, function(matchNo,horses){
                 $.each(horses, function(horseName){
